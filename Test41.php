296b9a2bf7c1aad1e899f0aef7d21a6afca179b8
DmitriySer <d1maser99@yandex.ru>
2022-09-01 22:01:44 +0400

    v5

---
+++ Test41.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace HexletBasics\DefineFunctions\ReturnInstruction;
+
+// BEGIN (write your solution here)
+
+$hurray = sayHurrayThreeTimes();
+
+function sayHurrayThreeTimes() {
+    return("hurray! hurray! hurray!");
+}
+
+// END
